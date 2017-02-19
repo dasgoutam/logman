50278ad04f9df3c16c1ccff96b681da708f13fb4
goutam2475 <goutam2475@gmail.com>
2017-02-19 22:33:43 +0530

    added stroke object record all keystrokes

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import pyxhook.pyxhook as pyxhook
 import time
 
-
+class Stroke:
+	def __init__(self, key, mname, time):
+		self.key = key
+		self.mname = mname
+		self.time = time
+	
+	
 # This function is called every time a key is presssed
 def kbevent(event):
     global running
+    global UpArray, DownArray
     # print key info
     tm = repr(time.time())
-    info = event.Key + "\t" + event.MessageName + "\t" + repr(time.time()) + "\n"
-    file.write(info)
+
+    keyEvent = Stroke(event.Key, event.MessageName, tm)
+    if keyEvent.mname == "key down":
+    	UpArray.append(keyEvent)
+    else:
+    	DownArray.append(keyEvent)
 
     # If the ascii value matches spacebar, terminate the while loop
     if event.Ascii == 13:
-        running = False
+    	if keyEvent.mname == "key up":   		
+	        running = False
 
+def printList(array):
+	for stroke in array:
+		print stroke.key, stroke.mname, stroke.time, "#"
+	print "##"
 
-# def kb1event(event):
-# 	global running
-# 	info = event.Key + "(up):" + str(time.time()) + " "
-# 	file.write(info)
-
+UpArray = []
+DownArray = []
 # Create hookmanager
 hookman = pyxhook.HookManager()
 # print time.time()
@@ -30,7 +43,7 @@ hookman.KeyUp = kbevent
 hookman.HookKeyboard()
 # Start our listener
 hookman.start()
-file = open('output.txt', 'w')
+# file = open('output.txt', 'w')
 # Create a loop to keep the application running
 running = True
 while running:
@@ -38,4 +51,16 @@ while running:
 
 # Close the listener when we are done
 hookman.cancel()
-file.close()
+
+
+print "Key Down:\n"
+# print printList(UpArray)
+for stroke in DownArray:
+	print stroke.key, stroke.mname, stroke.time, "#"
+print "###"
+# print "counter:", counter
+print len(UpArray)
+print "Key Up:\n"
+print printList(DownArray)
+print len(DownArray)
+# file.close()
